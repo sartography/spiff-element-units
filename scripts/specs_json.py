7ca888ff323d951d9719e1dad4b9d865284f674d
Jon Herron <jon.herron@yahoo.com>
2023-04-12 18:12:33 -0400

    Simplify the format, it'll be easy enough to convert to and from the backend requirements

--- scripts/specs_json.py
+++ scripts/specs_json.py
@@ -13,16 +13,6 @@ PROCESS_MODELS_DIR = "tests/data/process-models"
 def bpmn_test_case(suffix):
     return f"{PROCESS_MODELS_DIR}/test-cases/{suffix}"
 
-# TODO: as we add more do a convention where the folder, file and process id line up so
-# this can be assumed for the majority of cases
-PROCESS_IDS = {
-    bpmn_test_case("countries-and-cities/country_cities.bpmn"): "Process_sypm122",
-    bpmn_test_case("manual-tasks/manual_tasks.bpmn"): "Process_diu8ta2",
-    bpmn_test_case("no-tasks/no-tasks.bpmn"): "no_tasks",
-    bpmn_test_case("simple-call-activity/simple_call_activity.bpmn"): "Process_p4pfxhq",
-    bpmn_test_case("single-task/single_task.bpmn"): "SingleTask_Process",
-}
-
 SUPPORTING_FILES = {
     bpmn_test_case("simple-call-activity/simple_call_activity.bpmn"): [
         bpmn_test_case("single-task/single_task.bpmn"),
@@ -36,20 +26,10 @@ def _required_files(bpmn_file):
 def _to_dict(bpmn_file):
     parser = SpiffBpmnParser()
     bpmn_files = _required_files(bpmn_file)
-    process_id = PROCESS_IDS[bpmn_file]
     parser.add_bpmn_files(bpmn_files)
-    # TODO: backend make two find calls, one for the spec and one for the subprocesses
+    
     specs = parser.find_all_specs()
-
-    # spiffworkflow-backend does it like this, so
-    bpmn_process_spec = parser.get_spec(process_id)
-    subspecs = parser.get_subprocess_specs(process_id)
-
-    #return {k: SPEC_CONVERTER.convert(v) for k, v in specs.items()}
-    return {
-        "spec": SPEC_CONVERTER.convert(bpmn_process_spec),
-        "sub_processes": {k: SPEC_CONVERTER.convert(v) for k, v in subspecs.items()}
-    }
+    return {k: SPEC_CONVERTER.convert(v) for k, v in specs.items()}
 
 def _write_dict_as_json(bpmn_file, dct):
     # correct, this is not very robust
