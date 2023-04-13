9e980d2405c10f2646e37947bd9c56fe42a88fa8
Jon Herron <jon.herron@yahoo.com>
2023-04-13 11:36:48 -0400

    Test refactor

--- tests/integration/test_execute_specs_json.py
+++ tests/integration/test_execute_specs_json.py
@@ -25,38 +25,38 @@ def _workflow_from_specs_json(relname, process_id):
     top_level, subprocesses = _converted_specs(specs, process_id)
     return BpmnWorkflow(top_level, subprocesses)
 
+def _do_engine_steps(workflow):
+    workflow.do_engine_steps()
+
+def _test(relname, process_id, executor, result):
+    workflow = _workflow_from_specs_json(relname, process_id)
+    executor(workflow)
+        
+    assert workflow.is_completed()
+    assert workflow.data == result
+
 # TODO: test cases that need service tasks are not executed right now
 # TODO: add delegate to mock out responses to fix ^
 class ExecuteSpecJsonFilesTest(TestCase):
+    #
+    # these are kept as separate methods so unittest-parallel can rip them up
+    #
+    
     def test_no_tasks_executes(self):
-        workflow = _workflow_from_specs_json("no-tasks/no-tasks.json", "no_tasks")
-        workflow.do_engine_steps()
-        
-        assert workflow.is_completed()
-        assert workflow.data == {}
+        _test("no-tasks/no-tasks.json", "no_tasks", _do_engine_steps, {})
 
     def test_single_task_executes(self):
-        workflow = _workflow_from_specs_json("single-task/single_task.json", "SingleTask_Process")
-        workflow.do_engine_steps()
-        
-        assert workflow.is_completed()
-        assert workflow.data == {"x": 1}
+        _test("single-task/single_task.json", "SingleTask_Process", _do_engine_steps, {"x": 1})
 
     def test_simple_call_activity_executes(self):
-        workflow = _workflow_from_specs_json("simple-call-activity/simple_call_activity.json", "Process_p4pfxhq")
-        workflow.do_engine_steps()
-        
-        assert workflow.is_completed()
-        assert workflow.data == {"x": 1}
+        _test("simple-call-activity/simple_call_activity.json", "Process_p4pfxhq", _do_engine_steps, {"x": 1})
 
     def test_manual_tasks_executes(self):
-        workflow = _workflow_from_specs_json("manual-tasks/manual_tasks.json", "Process_diu8ta2")
+        def executor(workflow):
+            workflow.do_engine_steps()
+            workflow.get_ready_user_tasks()[0].complete()
+            workflow.do_engine_steps()
+            workflow.get_ready_user_tasks()[0].complete()
+            workflow.do_engine_steps()
 
-        workflow.do_engine_steps()
-        workflow.get_ready_user_tasks()[0].complete()
-        workflow.do_engine_steps()
-        workflow.get_ready_user_tasks()[0].complete()
-        workflow.do_engine_steps()
-        
-        assert workflow.is_completed()
-        assert workflow.data == {}
+        _test("manual-tasks/manual_tasks.json", "Process_diu8ta2", executor, {})
--- tests/test_sanity.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-
-from spiff_element_units.workflow_spec_creator import create_workflow_spec
-
-class CreateWorkflowSpecTest(TestCase):
-    def test_it_returns_a_workflow_spec_for_an_empty_workflow(self):
-        assert True
