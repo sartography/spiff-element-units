e6ce43beccf54acb68cde83a257be3eb91a52008
Jon Herron <jon.herron@yahoo.com>
2023-04-13 11:12:36 -0400

    Fill out test cases

--- tests/integration/test_execute_specs_json.py
+++ tests/integration/test_execute_specs_json.py
@@ -25,11 +25,37 @@ def _workflow_from_specs_json(relname, process_id):
     top_level, subprocesses = _converted_specs(specs, process_id)
     return BpmnWorkflow(top_level, subprocesses)
 
-# TODO: to leverage the unittest-parallel, is it better to have many small test suites?
-# does the number of files matter or just the TestCase classes?
+# TODO: test cases that need service tasks are not executed right now
+# TODO: add delegate to mock out responses to fix ^
 class ExecuteSpecJsonFilesTest(TestCase):
     def test_no_tasks_executes(self):
         workflow = _workflow_from_specs_json("no-tasks/no-tasks.json", "no_tasks")
+        workflow.do_engine_steps()
+        
+        assert workflow.is_completed()
+        assert workflow.data == {}
+
+    def test_single_task_executes(self):
+        workflow = _workflow_from_specs_json("single-task/single_task.json", "SingleTask_Process")
+        workflow.do_engine_steps()
+        
+        assert workflow.is_completed()
+        assert workflow.data == {"x": 1}
+
+    def test_simple_call_activity_executes(self):
+        workflow = _workflow_from_specs_json("simple-call-activity/simple_call_activity.json", "Process_p4pfxhq")
+        workflow.do_engine_steps()
+        
+        assert workflow.is_completed()
+        assert workflow.data == {"x": 1}
+
+    def test_manual_tasks_executes(self):
+        workflow = _workflow_from_specs_json("manual-tasks/manual_tasks.json", "Process_diu8ta2")
+
+        workflow.do_engine_steps()
+        workflow.get_ready_user_tasks()[0].complete()
+        workflow.do_engine_steps()
+        workflow.get_ready_user_tasks()[0].complete()
         workflow.do_engine_steps()
         
         assert workflow.is_completed()
